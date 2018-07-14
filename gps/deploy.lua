af12f560ea2d1b0c9e74c6839a1eff7857bc69de
Set <6942774+setzerbomb@users.noreply.github.com>
2018-07-14 05:25:29 -0300

    Update deploy.lua

--- gps/deploy.lua
+++ gps/deploy.lua
@@ -1,7 +1,7 @@
 --[[
 The MIT License (MIT)
 
-Copyright (c) 2018 Gabriel777
+Copyright (c) 2018 Setzerbomb
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
